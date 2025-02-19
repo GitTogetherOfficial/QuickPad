85bb268e1c41154698640735d42b12510cf80b47
TheAltening156 <90117489+TheAltening156@users.noreply.github.com>
2025-02-19 17:52:04 +0100

    hjkb
    j

--- obj/Debug/net8.0-windows/QuickPad.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/QuickPad.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuickPad")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9b79cd81ad3613116efb4b6e9386784a5a64245")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuickPad")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuickPad")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
